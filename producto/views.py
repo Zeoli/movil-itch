52e9437a7f356bfb3eec6ce59a797781873d8847
Toni Maldonado <zeoli.99.toni@gmail.com>
2016-01-03 19:38:35 -0500

    fix productos

--- producto/views.py
+++ producto/views.py
@@ -4,4 +4,5 @@ from .models import *
 
 def Main_view(request):
     productos = Producto.objects.all()
+    productos = list(productos)
     return render(request, 'index.html', {'productos': productos})
